fb0865de27623897661eb410fbfe1ffef2643b35
Maria <maria.meneses@eia.edu.co>
2025-04-02 09:18:11 -0500

    .

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,30 +1,42 @@
-import { IsString, IsNumber, IsOptional, IsUrl, Min, IsIn } from "class-validator";
+import {
+  IsString,
+  IsNumber,
+  IsOptional,
+  IsUrl,
+  Min,
+  IsIn,
+} from 'class-validator';
 
 export class CreateProductDto {
-    @IsString()
-    name: string;
+  @IsString()
+  name: string;
 
-    @IsNumber()
-    @Min(0)
-    price: number;
+  @IsNumber()
+  @Min(0)
+  price: number;
 
-    @IsString()
-    @IsOptional()
-    description?: string;
+  @IsString()
+  @IsOptional()
+  description?: string;
 
-    @IsUrl()
-    @IsOptional()
-    image?: string;
+  @IsUrl()
+  @IsOptional()
+  image?: string;
 
-    @IsString()
-    @IsIn(['Tejido Blando', 'Tejido Duro', 'Tejido Ocular'])
-    category: string;
+  @IsString()
+  @IsIn(['Tejido Blando', 'Tejido Duro', 'Tejido Ocular'])
+  category: string;
 
-    @IsString()
-    @IsIn(['Rehabilitación Ortopédica', 'Reparación de Heridas', 'Recuperación Visual', 'Mejora de la Función Cardíaca'])
-    use: string;
+  @IsString()
+  @IsIn([
+    'Rehabilitación Ortopédica',
+    'Reparación de Heridas',
+    'Recuperación Visual',
+    'Mejora de la Función Cardíaca',
+  ])
+  use: string;
 
-    @IsString()
-    @IsIn(['Ortopedia', 'Cardiología', 'Oftalmología', 'Cirugía Plástica'])
-    specialty: string;
+  @IsString()
+  @IsIn(['Ortopedia', 'Cardiología', 'Oftalmología', 'Cirugía Plástica'])
+  specialty: string;
 }
